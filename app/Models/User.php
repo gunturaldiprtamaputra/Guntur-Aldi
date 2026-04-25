615cbc581db5500b7e47432c62fee7f7bb45bb79
gunturaldiprtamaputra <ghokage007@gmail.com>
2026-04-25 19:16:00 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // ← tambah ini
 
 class User extends Authenticatable
 {
